350f7fe5caca93c41b9e6b7d8573a11599a7430f
Austin Sullivan <acs3ss@virginia.edu>
2019-03-23 14:32:11 -0400

    basic requests work

--- alexa/get_all_routes.py
+++ alexa/get_all_routes.py
@@ -22,27 +22,47 @@ agency_ids = [x['agency_id'] for x in response.json()['data']]
 payload = (('format',   'json'), ('agencies', ",".join(agency_ids)))
 response = requests.get(api_url + ROUTES, headers=headers, params=payload)
 
-all_routes = []
+alexa_all_routes = []
+google_all_routes = []
 
 for agency_id, routes in response.json()['data'].items():
     for route in routes:
-        name = {
+        alexa_name = {
             "name": {
                 "value": route["long_name"],
             }
         }
 
         if route["short_name"] != "" :
-            name["name"]["synonyms"] = [ route["short_name"] ]
+            alexa_name["name"]["synonyms"] = [ route["short_name"] ]
 
-        all_routes.append(name)
+        google_name = {
+            "value": route["long_name"],
+        }
 
-file_path = 'BusBuddy/models/en-US.json'
-data = json.load(open(file_path, 'r'))
+        if route["short_name"] != "" :
+            google_name["synonyms"] = [ route["short_name"] ]
 
+        alexa_all_routes.append(alexa_name)
+        google_all_routes.append(google_name)
+
+alexa_file_path = 'BusBuddy/models/en-US.json'
+google_file_path = '../BusLine.json'
+
+# write out Alexa json
+data = json.load(open(alexa_file_path, 'r'))
 for i in range(len(data['interactionModel']['languageModel']['types'])):
     if data['interactionModel']['languageModel']['types'][i]['name'] == "BusLine":
-        data['interactionModel']['languageModel']['types'][i]['values'] = all_routes
-
-with open(file_path, 'w') as outfile:
+        data['interactionModel']['languageModel']['types'][i]['values'] = alexa_all_routes
+with open(alexa_file_path, 'w') as outfile:
     json.dump(data, outfile)
+
+# write out Google json
+data = json.load(open(google_file_path, 'r'))
+if data['name'] == "BusLine":
+    data['entries'] = google_all_routes
+
+    with open(google_file_path, 'w') as outfile:
+        json.dump(data, outfile)
+else:
+    print("You've got the wrong Google json file")
