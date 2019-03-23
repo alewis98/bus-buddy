08814cebcc8e38e0614d45e310861d199518ea96
Andrew Lewis <anjale98@gmail.com>
2019-03-22 21:53:32 -0400

    Added basic alexa app handler

---
+++ alexa/handler.py
@@ -0,0 +1,34 @@
+def build_response(text):
+    response = {
+        'version': '1.0',
+        'response': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': text,
+            }
+        }
+    }
+    return response
+
+
+def on_launch(intent_request, session):
+    return build_response("Welcome to Bus Buddy")
+
+
+def on_intent(intent_request, session):
+    intent = intent_request["intent"]
+    intent_name = intent_request["intent"]["name"]
+    if intent_name == "GetNextBus":
+        text = "Getting Next Bus"
+    else:
+        raise ValueError("Invalid intent")
+    return build_response(text)
+
+
+def main(event, context):  
+    if event["request"]["type"] == "IntentRequest":
+        return on_intent(event["request"], event["session"])
+    elif event["request"]["type"] == "LaunchRequest":
+        return on_launch(event["request"], event["session"])
+    else:
+        return build_response("I think an error occurred")
