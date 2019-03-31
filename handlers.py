59fe7c825e90ccbe1bb0f33fc5a28680d1585bf2
Austin Sullivan <acs3ss@virginia.edu>
2019-03-31 01:36:28 -0400

    working for Google

--- handlers.py
+++ handlers.py
@@ -13,10 +13,40 @@ SEGMENTS = "segments"
 VEHICLES = "vehicles"
 STOPS = "stops"
 
-api_url = "https://transloc-api-1-2.p.rapidapi.com/"
-headers = {
-  "X-RapidAPI-Key": "49e8d6f922msh58d18d370a3dc27p12e16djsn3eb2f5483f33"
-}
+def make_request(request_type, agencies=None, geo_area=None, routes=None, stops=None):
+    if type(agencies) is list:
+        agencies = ",".join(agencies)  # convert list of agencies into string
+    if type(routes) is list:
+        routes = ",".join(routes)  # convert list of stops into string
+    if type(stops) is list:
+        stops = ",".join(stops)  # convert list of stops into string
+
+    payload = [('format',   'json'),
+               ('agencies', agencies) if agencies else None,
+               ('geo_area', geo_area) if geo_area else None,
+               ('routes', routes) if routes else None,
+               ('stops', stops) if stops else None
+               ]
+    payload = [x for x in payload if x is not None]
+
+    api_url = "https://transloc-api-1-2.p.rapidapi.com/"
+    headers = {
+      "X-RapidAPI-Key": "49e8d6f922msh58d18d370a3dc27p12e16djsn3eb2f5483f33"
+    }
+
+    response = requests.get(api_url + request_type,
+                            headers=headers,
+                            params=payload)
+
+    if not response.ok:
+        print("Request errored with status code", response.status_code)
+        print("Reason:", response.reason)
+
+    if not response.json().get('data'):
+        print("Request returned no data. Payload:", payload)
+        return None
+
+    return response.json()['data']
 
 
 def build_geo_area(latitude, longitude, radius=10000):
@@ -26,9 +56,9 @@ def build_geo_area(latitude, longitude, radius=10000):
 
 def uncouple_geo_area(geo):
     return {
-        "latitude" : float(geo[:geo.find(",")]),
+        "latitude": float(geo[:geo.find(",")]),
         "longitude": float(geo[geo.find(",")+1:geo.find("|")]),
-        "radius"   : int(geo[geo.find("|")+1:])
+        "radius": int(geo[geo.find("|")+1:])
     }
 
 
@@ -38,11 +68,16 @@ def find_by_key(key_id, field, entities):
         print("key_id:", key_id, "field:", field, "entities:", entities)
         return
 
+    if not key_id:
+        print("Trying to find with None key_id")
+        print("key_id:", key_id, "field:", field, "entities:", entities)
+        return
+
     for entity in entities:
         if entity[field] == key_id:
             return entity
 
-    print("couldn't find", key_id)
+    print("Couldn't find", key_id)
     print("key_id:", key_id, "field:", field, "entities:", entities)
 
 
@@ -52,36 +87,9 @@ def get_bus_fullness(vehicle):
     except:
         return ""
 
-def get_agencies(geo_area=None, agencies=None):
-    payload = [ ('format',   'json'), \
-                ('agencies', agencies) if agencies else None, \
-                ('geo_area', geo_area) if geo_area else None \
-                ]
-    payload = [x for x in payload if x is not None]
-
-    response = requests.get(api_url + AGENCIES, headers=headers, params=payload)
-
-    if len(response.json()['data']) == 0:
-        print("NO AGENCY FOUND")
-        return None
-
-    return response.json()['data']
-
-
-def get_routes(agency_id, geo_area=None):
-    payload = [ ('format',   'json'), \
-                ('agencies', agency_id), \
-                ('geo_area', geo_area) if geo_area else None \
-                ]
-
-    payload = [x for x in payload if x is not None]
-
-    response = requests.get(api_url + ROUTES, headers=headers, params=payload)
-    return response.json()['data'].get(agency_id)
-
 
 def get_route(agency_id, route_name, geo_area=None):
-    routes = get_routes(agency_id, geo_area=geo_area)
+    routes = make_request(ROUTES, agencies=agency_id, geo_area=geo_area)
     return find_by_key(route_name, 'long_name', routes)
 
 
@@ -95,53 +103,28 @@ def distance_between(lat_1, lon_1, lat_2, lon_2):
     dist = math.acos(dist)
     dist = math.degrees(dist)
     dist = dist * 69.06         # 69.09 = circumference of earth in miles / 360 degrees
-
     return dist
 
 
-def get_stops(agency_id, geo_area=None, route_id=None):
-    payload = [ ('format',   'json'), \
-                ('agencies', agency_id), \
-                ('geo_area', geo_area) if geo_area else None \
-                ]
-    payload = [x for x in payload if x is not None]
+def get_route_stops(agency_id, route_id):
+    response = make_request(STOPS, agencies=agency_id, routes=[route_id])
 
-    response = requests.get(api_url + STOPS, headers=headers, params=payload)
-    if route_id:
-        stops = []
-        for stop in response.json()['data']:
-            if route_id in stop['routes']:
-                stops.append(stop)
-        return stops
-    else:
-        return response.json()['data']
+    stops = []
+    for stop in response:
+        if route_id in stop['routes']:
+            stops.append(stop)
+    return stops
 
 
-def sort_stops(agency_id, geo_area, stops=None):
-    if not stops:
-        stops = get_stops(agency_id, geo_area)
+def sort_stops(stops, location):
+    if type(location) is str:
+        location = uncouple_geo_area(location)
+    elif type(location) is not dict:
+        print("Invalid location format")
+        return None
 
-    geo_dict = uncouple_geo_area(geo_area)
-
-    closest_stop = sorted(stops, key=lambda x: distance_between(x['location']['lat'], x['location']['lng'], geo_dict['latitude'], geo_dict['longitude']))
+    closest_stop = sorted(stops, key=lambda x: distance_between(x['location']['lat'], x['location']['lng'], location['latitude'], location['longitude']))
     return closest_stop
-
-
-def get_vehicles(agency_id, routes=None, geo_area=None):
-    payload = [ ('format',   'json'), \
-                ('agencies', agency_id), \
-                ('geo_area', geo_area) if geo_area else None, \
-                ('routes', ",".join(routes)) if routes else None \
-                ]
-    payload = [x for x in payload if x is not None]
-
-    response = requests.get(api_url + VEHICLES, headers=headers, params=payload)
-    vehicles = response.json()['data'][agency_id]
-
-    print([vehicle['vehicle_id'] for vehicle in vehicles])
-    print("There are", len(vehicles), "vehicles")
-
-    return vehicles
 
 
 def get_vehicle_current_stop(vehicle):
@@ -151,25 +134,14 @@ def get_vehicle_current_stop(vehicle):
         return None
 
 
-def get_arrival_estimates(agency_id, routes=None, stops=None):
-    payload = [ ('format',   'json'), \
-                ('agencies', agency_id), \
-                ('routes', ",".join(routes)) if routes else None, \
-                ('stops', ",".join(stops)) if stops else None \
-                ]
-    payload = [x for x in payload if x is not None]
-
-    response = requests.get(api_url + ARRIVAL_ESTIMATES, headers=headers, params=payload)
-    return response.json()['data']
-
-
 def get_arrivals_for_stop(agency_id, stop_id, routes=None):
     print("ROUTES", routes)
     print("STOP_ID", stop_id)
-    arrivals = get_arrival_estimates(agency_id, routes=routes, stops=[stop_id])
+
+    arrivals = make_request(ARRIVAL_ESTIMATES, agencies=agency_id, routes=routes, stops=stop_id)
     print("ARRIVALS", arrivals)
     stop_arrivals = find_by_key(stop_id, "stop_id", arrivals)
-    return stop_arrivals['arrivals'] if stop_arrivals else None
+    return stop_arrivals.get('arrivals')
 
 
 def get_arrival_time_estimates(arrivals):
@@ -214,6 +186,10 @@ def get_one_bus_arrival_text(arrival_time, bus_line, stop_name):
 def convert_address_to_coordinates(address):
     print("ADDRESS:", address)
     api_key = "AIzaSyDWCnuBAOcZHyNDzRTa6JC9PtDRnML6UQI"
+    if type(address) is not dict:
+        print("Invalid attempt to convert address:", address)
+        return None
+
     try:
         address_string = "+".join([address['addressLine1'], \
                                    address['city'], address['stateOrRegion'], \
@@ -222,38 +198,49 @@ def convert_address_to_coordinates(address):
         address_string = "+".join(address.values())
     response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address_string + "&key=" + api_key)
     if len(response.json()['results']) == 0:
+        print("No location could be determined from this address:", address)
         return None
     else:
         return response.json()['results'][0]['geometry']['location']
 
 
-def get_bus_line_info(intent_request, bus_lines, geo_area, where=False):
+def get_next_bus(intent_request, bus_lines, geo_area, where=False):
     text = ""
 
-    agencies = get_agencies(geo_area=geo_area)
+    if type(bus_lines) is str:
+        bus_lines = [bus_lines]  # we want to loop through bus routes, not characters of a string
+
+    agencies = make_request(AGENCIES, geo_area=geo_area)
     if not agencies:
-        return "That route is very far from your location"
+        return "We can't find any routes near you"
     print("AGENCIES:", agencies)
     matched_route = None
 
     for ag in agencies:
+        ag_id = ag['agency_id']
         for bus_line in bus_lines:
-            route = get_route(agency_id=ag['agency_id'], route_name=bus_line)
-            if route:
-                matched_route = route
-                agency = ag
-                agency_id= ag['agency_id']
-                break
+            routes = make_request(ROUTES, agencies=ag_id).get(ag_id)
+            route = find_by_key(bus_line, 'long_name', routes)
+            if not route:
+                print("Couldn't find route", bus_line, "in agency", ag_id)
+                continue
+            matched_route = route
+            route_name = matched_route['long_name']
+            agency = ag
+            agency_id = ag_id
+            break
 
     if not matched_route:
-        return "I couldn't find any buses running that route right now"
+        return "I couldn't find any routes by that name near you"
 
-    print("AGENCY:", agency)
+    print("AGENCY:", agency.get('long_name'))
     print("MATCHED ROUTE:", matched_route.get('route_id'))
-    stops = sort_stops(agency_id, geo_area=geo_area)
+
+    stops = make_request(STOPS, agencies=agency_id, geo_area=geo_area, routes=matched_route['route_id'])
+    sorted_stops = sort_stops(stops, geo_area)
 
     nearest_stop = None
-    for stop in stops:
+    for stop in sorted_stops:
         if matched_route['route_id'] in stop['routes']:
             nearest_stop = stop
             break
@@ -261,22 +248,23 @@ def get_bus_line_info(intent_request, bus_lines, geo_area, where=False):
         return "I couldn't find any stops near you for the " + bus_line
 
     print("Nearest stop:", nearest_stop['stop_id'])
-    estimates = get_arrivals_for_stop(agency_id, stop_id=nearest_stop['stop_id'], routes=[matched_route['route_id']])
+    estimates = get_arrivals_for_stop(agency_id, stop_id=nearest_stop['stop_id'], routes=matched_route['route_id'])
+
     print("ESTIMATES:", estimates)
-    if not estimates or len(estimates) == 0:
-        return "There are no upcoming " + matched_route['long_name'] + " buses near you."
+    if not estimates:
+        return "There are no upcoming " + route_name + " buses near you."
 
     arrival_time_deltas = get_arrival_time_estimates(estimates)
-    print("Arrival time deltas", arrival_time_deltas)
 
     if where:
         your_bus_id = estimates[0]['vehicle_id']
         print("YOUR BUS ID:", your_bus_id)
-        vehicles = get_vehicles(agency_id, routes=[matched_route['route_id']])
+        response = make_request(VEHICLES, agencies=agency_id, routes=matched_route['route_id'])
+        vehicles = response.get(agency_id)
 
         your_bus = find_by_key(your_bus_id, "vehicle_id", vehicles)
         vehicle_current_stop = get_vehicle_current_stop(your_bus)
-        current_stop = find_by_key(vehicle_current_stop['stop_id'], 'stop_id', stops)
+        current_stop = find_by_key(vehicle_current_stop.get('stop_id'), 'stop_id', sorted_stops)
 
         if not current_stop:
             return "Can't find that bus right now"
@@ -328,9 +316,9 @@ def on_intent_alexa(intent_request, session, addr):
     bus_names = [bus['value']['name'] for bus in bus_lines]
 
     if intent_name == "GetNextBus":
-        text = get_bus_line_info(intent_request, bus_names, geo_area=geo_area, where=False)
+        text = get_next_bus(intent_request, bus_names, geo_area=geo_area, where=False)
     elif intent_name == "WhereIsBus":
-        text = get_bus_line_info(intent_request, bus_names, geo_area=geo_area, where=True)
+        text = get_next_bus(intent_request, bus_names, geo_area=geo_area, where=True)
     else:
         return build_response_alexa("Invalid Intent")
     return build_response_alexa(text)
@@ -347,34 +335,35 @@ def on_intent_google(intent_request):
     print("PARAMS:", params)
     request_type = params['request_type']
 
-    # coordinates for Whyburn:          latitude=38.0294814, longitude=-78.5193463
+    # coordinates for Whyburn:          latitude=38.0294814, longitude=-78.5193463  '
+    #                                   geo_area='38.0294814,-78.5193463|10000'
     # coordinates for William and Mary: latitude=37.271674, longitude = -76.7155667
     latitude=38.0294814
     longitude=-78.5193463
-    if "location" in params.keys():
-        location = params['location']
-        print("ADDRESS DETECTED:", location)
-
-        if type(location) == type(""):
-            location = {'addressLine1': "216 Johnson", \
-                        'city': "Charlottesvile", \
-                        'stateOrRegion': "Virginia", \
-                        'postalCode': "22904-2120", \
-                        'countryCode': "US"
-                        }
-
-        location = convert_address_to_coordinates(location)
-        print("ADDRESS RESOLVED TO:", location)
-        if location:
-        	latitude  = location['lat']
-        	longitude = location['lng']
+    # if "location" in params.keys():
+    #     location = params['location']
+    #     print("ADDRESS DETECTED:", location)
+    #
+    #     if type(location) == type(""):
+    #         location = {'addressLine1': "216 Johnson", \
+    #                     'city': "Charlottesvile", \
+    #                     'stateOrRegion': "Virginia", \
+    #                     'postalCode': "22904-2120", \
+    #                     'countryCode': "US"
+    #                     }
+    #
+    #     location = convert_address_to_coordinates(location)
+    #     print("ADDRESS RESOLVED TO:", location)
+    #     if location:
+    #     	latitude  = location['lat']
+    #     	longitude = location['lng']
     geo_area = build_geo_area(latitude=latitude, longitude=longitude)
 
     if intent['displayName'] == "GetNextBus":
         if request_type == "when":
-            text = get_bus_line_info(intent_request, [params['bus_line']], geo_area=geo_area, where=False)
+            text = get_next_bus(intent_request, [params['bus_line']], geo_area=geo_area, where=False)
         elif request_type == "where":
-            text = get_bus_line_info(intent_request, [params['bus_line']], geo_area=geo_area, where=True)
+            text = get_next_bus(intent_request, [params['bus_line']], geo_area=geo_area, where=True)
         else:
             raise ValueError("Invalid request")
     else:
